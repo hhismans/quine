8f2c5ffe85ff4ab9a0eb5e3c3879a4f012daac74
Hugo HISMANS <hhismans@e2r10p20.42.fr>
2017-06-02 20:25:30 +0200

    add comment ex01

--- ex01/main.c
+++ ex01/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
-#define DATA "#include <stdio.h>%c#define DATA %c%s%c%c#define MAIN int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw%c);fprintf(file, DATA, 10, 34, DATA, 34, 10, 34,34,34,34,10,10);}%cMAIN%c"
-#define MAIN int main(){FILE *file = fopen("Grace_kid.c", "w");fprintf(file, DATA, 10, 34, DATA, 34, 10, 34,34,34,34,10,10);}
+/*
+	This program will write his source code in Grace_kid.c
+*/
+#define DATA "#include <stdio.h>%c/*%c%cThis program will write his source code in Grace_kid.c%c*/%c#define DATA %c%s%c%c#define MAIN int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw%c);fprintf(file, DATA, 10,10,9,10,10, 34, DATA, 34, 10, 34,34,34,34,10,10);}%cMAIN%c"
+#define MAIN int main(){FILE *file = fopen("Grace_kid.c", "w");fprintf(file, DATA, 10,10,9,10,10, 34, DATA, 34, 10, 34,34,34,34,10,10);}
 MAIN
