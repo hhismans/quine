3f0d69ca9a30078b879084d949ae4965403cfd7d
Hugo HISMANS <hhismans@e3r13p5.42.fr>
2017-07-03 17:53:00 +0200

    almost finshed

---
+++ ex00ml/main.ml
@@ -0,0 +1,2 @@
+(*Ex00 : Colleen*)
+let () = (*resume : (fun p) format*) (fun p -> Printf.printf p (string_of_format p)) "(*Ex00 : Colleen*)\nlet () = (*resume : (fun p) format*) (fun p -> Printf.printf p (string_of_format p)) %S;;\n";;
--- ex01ml/main.ml
+++ ex01ml/main.ml
@@ -1,1 +1,2 @@
-let () = let out = open_out "Grace_kid.ml" in (fun p -> Printf.fprintf out p (string_of_format p)) "let () = let out = open_out \"Grace_kid.ml\" in (fun p -> Printf.fprintf out p (string_of_format p)) %S;;\n";;
+(*Ex01 Grace_kid*)
+let () = let out = open_out "Grace_kid.ml" in (fun p -> Printf.fprintf out p (string_of_format p)) "(*Ex01 Grace_kid*)\nlet () = let out = open_out \"Grace_kid.ml\" in (fun p -> Printf.fprintf out p (string_of_format p)) %S;;\n";;
