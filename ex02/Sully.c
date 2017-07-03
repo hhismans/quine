a5c5dc0644cee54216fafea51b693c16ac5777e2
Hugo HISMANS <hhismans@e3r13p1.42.fr>
2017-07-03 09:17:16 +0200

    ex02 almost finished

---
+++ ex02/Sully.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define Q(x) char s[1000]=#x;x
+Q(int main() {int i = 5;s[20]--;char name[10] = "Sully_X";name[6] = s[20];if (i){fprintf(fopen(name, "w"),"#include<stdio.h>\n#define Q(x) char s[1000]=#x;x\nQ(%s)\n",s);}})
+
+//int main()
+//{
+//	char s[1000] = "int main(){ i = 6";
+//	s[16]--;
+//	printf("%s", s);
+//}
