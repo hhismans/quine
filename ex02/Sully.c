78f56caaadd6f6064b060d0bcd93160c9acd0e30
Hugo HISMANS <hhismans@e3r13p1.42.fr>
2017-07-03 09:43:09 +0200

    stager cest bien commit cest mieux

--- ex02/Sully.c
+++ ex02/Sully.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-#define Q(x) char s[1000]=#x;x
-Q(int main() {int i = 5;s[20]--;char name[10] = "Sully_X";name[6] = s[20];if (i){fprintf(fopen(name, "w"),"#include<stdio.h>\n#define Q(x) char s[1000]=#x;x\nQ(%s)\n",s);}})
+#include<string.h>
+#include<unistd.h>
+#define Q(x) char s[]=#x;x
+Q(int main() {int i = 5;s[20]--;char compil[100] = "/usr/bin/gcc ";char name[] = "Sully_X.c";name[6] = s[20];if (i){fprintf(fopen(name, "w"),"#include<stdio.h>\n#define Q(x) char s[]=#x;x\nQ(%s)\n",s);execle(compil,"-o hey", name);}})
 
 //int main()
 //{
