93277c8736c48e79a9ad6123e9ae9d3dc379e04a
Hugo HISMANS <hhismans@e3r13p5.42.fr>
2017-07-03 17:52:00 +0200

    final mais ca plante

--- ex02ml/main.ml
+++ ex02ml/main.ml
@@ -1,4 +1,4 @@
-(*Ex02 Grace_kid*)
+(*Ex02 Sully*)
 let index = 5
 let name = ("Sully_" ^ string_of_int (index - 1))
-let () = let out = open_out (name ^ ".ml") in (fun p i -> Printf.fprintf out p i (string_of_format p)) "(*Ex01 Grace_kid*)\nlet () = let index = %d in let out = open_out \"Grace_kid.ml\" in (fun p -> Printf.fprintf out p (string_of_format p)) %S;;\n" (index - 1);ignore((Sys.command ("ocamlopt -o " ^ name ^ " " ^ name ^ ".ml")));ignore((Sys.command ("./" ^ name)));;
+let () = if (index > 0) then ( let out = open_out (name ^ ".ml") in (fun p i -> Printf.fprintf out p i (string_of_format p)) "(*Ex02 Sully*)let index = 5\nlet name = ("Sully_" ^ string_of_int (index - 1))\nlet () = if (index > 0) then ( let out = open_out (name ^ \".ml\") in (fun p i -> Printf.fprintf out p i (string_of_format p)) %S (index - 1);ignore((Sys.command (\"ocamlopt -o \" ^ name ^ \" \" ^ name ^ \".ml\")));ignore((Sys.command (\"./\" ^ name))));;" (index - 1);ignore((Sys.command ("ocamlopt -o " ^ name ^ " " ^ name ^ ".ml")));ignore((Sys.command ("./" ^ name))));;
